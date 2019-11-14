1d6a777b37289c4238b27a3de8e57e0d8a543b70
Mohit Sharma <mohitsharma44@gmail.com>
2019-11-14 14:39:33 -0800

    Added support for login page, passing vars to index and prototype for redis

--- app.py
+++ app.py
@@ -3,20 +3,70 @@
 
 import os
 import json
+import pathlib
 import tornado
 import tornado.web
 import tornado.ioloop
 import tornado.httpserver
+from redis.sentinel import Sentinel
 from tornado.log import app_log as log
 from tornado.options import define, options, parse_command_line
 
+# redis_master_password = os.getenv('REDIS_MASTER_PASSWORD')
+# if not redis_master_password:
+#     redis_master_password = Path('/run/secrets/redis-master-password').read_text().strip()    
+base_dir = os.path.dirname(__file__)
+static_dir = os.path.join(base_dir, "static")
+invitations_dir = os.path.join(static_dir, "images/invitations/test/high/")
+
 define("port", default=8080, help="run on the given port", type=int)
+
+def get_from_redis(rsvp):
+    """
+    Get the data corresponding to the rsvp code
+    """
+    # We want to connect to a different replica everytime
+    # we need to run a query
+    try:
+        sentinel = Sentinel([('redis-sentinel', 16379)],
+                            socket_timeout=0.1,
+                            password=redis_master_password)
+        replica = sentinel.slave_for('master-node')
+        response = replica.hgetall(rsvp)
+    except Exception as ex:
+        print("Error fetching response for rsvp code: {}\n {}".format(rsvp, ex))
+        response = False
+    finally:
+        return response
 
 class BaseHandler(tornado.web.RequestHandler):
 
     def get_current_user(self):
         return self.get_secure_cookie("username")
 
+class LoginHandler(BaseHandler):
+
+    @tornado.gen.coroutine
+    def get(self):
+        print('Got get request for login')
+        self.render('login.html')
+
+    @tornado.gen.coroutine
+    def post(self):
+        print('Got post request for login')
+        # grab the username based on rsvp code
+        #user_data = get_from_redis(self.get_argument("rsvp-code"))
+        user_data = {'Name': 'Foo Bar', 'Cat': 1, 'Photo': 0,
+                     'RSVP': 1, 'Contact No': '1234567890'}
+        self.set_secure_cookie("rsvp", self.get_argument("rsvp_code"))
+        self.set_secure_cookie('username', user_data['Name'])
+        print("Stored username: {} and rsvp: {}".format(user_data['Name'],
+                                                        self.get_argument("rsvp_code")))
+        # self.set_secure_cookie('cat', user_data['Cat'])
+        # self.set_secure_cookie('photo', user_data['Photo'])
+        # self.set_secure_cookie('rsvp', user_data['RSVP'])
+        # self.set_secure_cookie('contact_no', user_data['Contact No'])
+        self.redirect("/invitation/")
 
 class DashHandler(BaseHandler):
 
@@ -43,13 +93,19 @@ class InvitationHandler(BaseHandler):
 
     @tornado.gen.coroutine
     def get(self):
-        log.info("got request for InvitationHandler")
-        self.render('invitation.html')
+        log.info("got request for InvitationHandler from {}".format(self.get_current_user()))
+        self.render('invitation.html',
+                    guest_name='foo Bar baz and family what else', category_name='cat1',
+                    image_links=[
+                        os.path.join('/', invitations_dir, 'first.png'),
+                        os.path.join('/', invitations_dir, 'second.png'),
+                        os.path.join('/', invitations_dir, 'cat1.png')
+                        ])
 
     @tornado.gen.coroutine
     def post(self):
         log.warn("I shouldn't be receiving post requests for invitationhandler")
-        
+
 class UserTableHandler(BaseHandler):
 
     @tornado.gen.coroutine
@@ -90,7 +146,6 @@ class DBHandler(BaseHandler):
 
 class Application(tornado.web.Application):
     def __init__(self):
-        base_dir = os.path.dirname(__file__)
         settings = {
             "cookie_secret": "123", # Change this in prod
             "login_url": "/auth/login",
@@ -100,6 +155,7 @@ class Application(tornado.web.Application):
             "xsrf_cookies": False,
         }
         tornado.web.Application.__init__(self, [
+            tornado.web.url(r'/login/.*', LoginHandler, name='loginhandler'),
             tornado.web.url(r'/invitation/.*', InvitationHandler, name='invitationhandler'),
             # Testing photoviewer
             tornado.web.url(r'/photoviewer/.*', PhotoHandler, name='photohandler'),
